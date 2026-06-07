632bcc8641549838fec8939bae0bcc5f9290e122
ChristoferRSouza <christofer.ribeirodesouza@gmail.com>
2026-06-06 23:37:51 -0300

    Add initial implementation of interactive bingo panel with sponsor integration

---
+++ patrocinadores.js
@@ -0,0 +1,8 @@
+// Arquivo: patrocinadores.js
+
+const sponsorConfig = {
+    10: { name: "Patrocinador 10", logo: "patrocinadores/PatrocinadorX.png" },
+    25: { name: "Patrocinador 25", logo: "patrocinadores/PatrocinadorX.png" },
+    30: { name: "Patrocinador 30", logo: "patrocinadores/PatrocinadorX.png" },
+    60: { name: "Patrocinador 60", logo: "patrocinadores/PatrocinadorX.png" },
+};
